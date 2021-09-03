06033250d2f1f09f26b4fe49bb515e9fd721e083
Georjane <pzgeorjane@gmail.com>
2021-09-03 06:11:25 +0100

    Run migrations and add user attribute validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    # adds virtual attributes for authentication
+    has_secure_password
+    # validates email
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_150529) do
+ActiveRecord::Schema.define(version: 2021_09_03_050352) do
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
     t.integer "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
